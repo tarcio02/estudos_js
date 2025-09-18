db31ae42bd69cb353abca12e0234c3c19b7617d7
Tarcio Teixeira <tarciogbteixeira@gmail.com>
2025-09-18 08:58:19 -0300

    salvando arquivo

--- 02-scopo-hoisting-this/conteudo/this.js
+++ 02-scopo-hoisting-this/conteudo/this.js
@@ -2,3 +2,15 @@
 
 console.log(this)
 // refere-se ao objeto window ou no caso do node.js o ambiente de servidor
+
+// this com estrict mode 
+
+'use strict'
+
+    function mostrarThis() {
+        console.log(this)
+    }
+
+    mostrarThis() // A saída será undefined
+
+    
